9149b9035b79064ea94d0e93098d5968613dc9d3
mezeiser <mezardini@gmail.com>
2023-10-15 13:26:58 +0100

    worked on the fixture list as it was changed from our client

--- scoracle/settings.py
+++ scoracle/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m4e6jkhpvmsg8gaj(fo=ewayt2=r%tj+y7zhot4&dn&##y7#)#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
