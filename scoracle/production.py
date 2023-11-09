cff29901948ed9f65989fea2062517f6591a2629
mezeiser <mezardini@gmail.com>
2023-11-09 05:29:06 +0100

    adding a new method of displaying games

---
+++ scoracle/production.py
@@ -0,0 +1,30 @@
+from scoracle.settings import *
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['scoracle.onrender.com', '127.0.0.1']
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
+        'USER': env('USER'),
+
+    }
+}
+
+
+# _psycopg2
