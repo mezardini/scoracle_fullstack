d121764c1a46cfd1e28298afca1360e840c97675
mezeiser <mezardini@gmail.com>
2023-11-25 07:21:21 +0100

    changed the home page and added a link to predict a specific league in outcome.html

--- base/urls.py
+++ base/urls.py
@@ -3,12 +3,13 @@ from . import views
 from .views import LeaguePrediction, XPrediction
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('predictleague', views.home, name='home'),
     path('leagueprediction/', LeaguePrediction.as_view(), name="leagueprediction"),
     # path('matchprediction/', MatchPrediction.as_view(), name="matchprediction"),
     path('contact/', views.contact, name='contact'),
     path('xprediction/', XPrediction.as_view(), name="xprediction"),
-    path('outcome/', views.outcome, name='outcome'),
+    path('', views.outcome, name='outcome'),
     path('xpredict/', views.xpredict, name='xpredict'),
     path('pastpredictions/', views.pastpredictions, name='pastpredictions'),
+    path('vip/', views.vipsection, name='vipsection'),
 ]
