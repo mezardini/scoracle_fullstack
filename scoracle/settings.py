fdb47f2da54193c80a8710f2300c4f137fa06441
mezeiser <mezardini@gmail.com>
2024-06-12 14:38:26 +0100

    added more countries and moving to vercel

--- scoracle/settings.py
+++ scoracle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['scoracle.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['scoracle.onrender.com', '127.0.0.1', '*']
 
 
 # Application definition
